f1e34f9d6ae8df8d3ab7396767bbb32ef2562b5f
robertDurst <me@robdurst.com>
2026-05-16 13:37:41 -0600

    update index.html and status.html to enhance project description and embed Datadog dashboard

--- docs/status.html.pm
+++ docs/status.html.pm
@@ -6,4 +6,4 @@
 
 ◊cyan{We dogfood ◊link["https://caffeine-lang.run/"]{Caffeine} on our own infrastructure. The dashboard below is rendered from SLO definitions written in Caffeine and shipped to Datadog.}
 
-◊datadog-embed["https://p.datadoghq.com/sb/embed/5132f4c8-409d-11ee-9683-da7ad0900002-679fca999eae93e17c4c020056700d2e"]
+<iframe src="https://p.datadoghq.com/sb/embed/5132f4c8-409d-11ee-9683-da7ad0900002-679fca999eae93e17c4c020056700d2e" title="Dashboard Name" width="800" height="800"></iframe>
