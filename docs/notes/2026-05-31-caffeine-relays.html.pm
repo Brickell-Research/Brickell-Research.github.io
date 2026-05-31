9f41312d26fc63e04d998f9f711e522ed39d9531
robertDurst <me@robdurst.com>
2026-05-31 17:25:58 -0600

    update relay post

--- docs/notes/2026-05-31-caffeine-relays.html.pm
+++ docs/notes/2026-05-31-caffeine-relays.html.pm
@@ -20,17 +20,34 @@ The specific relay we are starting with is Langfuse to Datadog. By setting this 
 
 ◊h2{Syntax}
 
+So consider how measurements look today:
 ◊code-block{
-"langfuse_to_datadog" (Relay):
-  from   "langfuse"
+"some measurement":
+    Requires { service_name: String }
+    Provides {
+        indicators: {
+            numerator: "sum:successes{$$service->service_name$$}",
+            denominator: "sum:total{$$service->service_name$$}"
+        },
+        evaluation: "numerator / denominator"
+    }
+}
+
+Here we had to define both the numerator and denominator queries and even though we are attempting to write our own datadog query parser library, since it is not quite documented, it is actually fairly likely most folks will successfully compile expectations to only have them fail to apply.
+
+Consider now the relay version of this measurement:
+◊code-block{
+"some measurement":
+  Requires { service_name: String }
+  from   "some source"
   to     "datadog"
   every  15m
-  metric_prefix "langfuse.scores"
-  scorers {
-    "helpfulness": Numeric,
-    "safe":        Boolean
-  }
+  metric_prefix "some_source.hits"
 }
+
+Now this is definitely not the final version, but the beauty here is that with the relay, the user does not need to write a query at all. Instead they just define their intent and the relay takes care of the rest.
+
+Today folks would have to (a) figure out how to do the plumbing themselves AND then (b) figure out the query syntax to specify in the measurement definition to more or less just write Caffeine... but with the relay we just handle that all for folks. And as bonus avoid the unpleasant experience of writing Datadog queries by hand.
 
 ◊h2{The Artifact}
 
